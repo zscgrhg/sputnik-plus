2fdc865f668f18e8bea0f44d9fd0ce15475768e4
zscgrhg <zscgrhg@qq.com>
2020-04-13 23:45:52 +0800

    better

--- src/main/java/com/zte/sputnik/extension/InvokeStaicHelper.java
+++ src/main/java/com/zte/sputnik/extension/InvokeStaicHelper.java
@@ -1,13 +1,19 @@
 package com.zte.sputnik.extension;
 
 import com.alibaba.ttl.TransmittableThreadLocal;
+import com.zte.sputnik.instrument.BMUtil;
+import com.zte.sputnik.lbs.LoggerBuilder;
+import com.zte.sputnik.util.JsonUtil;
 import org.jboss.byteman.rule.Rule;
+import shade.sputnik.org.slf4j.Logger;
 
 import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class InvokeStaicHelper {
+    private static final Logger LOGGER = LoggerBuilder.of(InvokeStaicHelper.class);
     private static final TransmittableThreadLocal<Map<String, Object>> INVOKE_STATIC_TTL = new TransmittableThreadLocal();
 
     Rule rule;
@@ -27,7 +33,17 @@ public class InvokeStaicHelper {
     }
 
     public  boolean hasMethod(String methodSignure) {
-        return Optional.ofNullable(INVOKE_STATIC_TTL.get()).orElse(Collections.emptyMap()).containsKey(methodSignure);
+        Map<String, Object> mocks = Optional.ofNullable(INVOKE_STATIC_TTL.get()).orElse(Collections.emptyMap());
+
+        boolean b = mocks.containsKey(methodSignure);
+        LOGGER.debug("has method {} : {}", b,methodSignure);
+
+        if(!b){
+            LOGGER.debug("not match has method {} : {}", b,methodSignure);
+            LOGGER.debug("mocks {} \n", mocks.keySet().stream().collect(Collectors.joining("\n")));
+            LOGGER.debug("methodSignure: {} \n", methodSignure);
+        }
+        return b;
     }
 
     public  Object getMock(String methodSignure) {
